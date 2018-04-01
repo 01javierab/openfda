b660f21d46ff9e8257b48460865a2cb45f437bb9
01javierab <01javierab@gmail.com>
2018-04-01 20:11:17 +0200

    openfda-2 Ejercicio 3

---
+++ openfda-2/EjPrrograma3.py
@@ -0,0 +1,35 @@
+import http.client
+import json
+
+skip_number=0
+SERVER = "api.fda.gov"
+RESOURCE = '/drug/label.json?limit=100&skip='+str(skip_number)+'&search=substance_name:"ASPIRIN"'
+headers={'User-Agent':'http-client'}
+#headers le dices el nombre de tu navegador (chrome, firefox)
+
+while skip_number!=100:
+    conexion = http.client.HTTPSConnection(SERVER)
+    # Creo una conexion con la pagina fda. Abro un canal con esa pagina
+    conexion.request("GET", RESOURCE, None, headers)
+    # Mando esa peticion de tipo get, y le meto el recurso que me interesa
+    r1 = conexion.getresponse()
+    # Es la respuesta de informacion de openfda
+    if r1.status ==404:
+        print("Atencion usuario, Recurso: {} no existe o no encontrado".format(RESOURCE))
+        exit(1)
+    else:
+        label = r1.read().decode("utf-8")
+        # Lees el contenido de la respuesta y lo decodificas a utf8 para que se vean tildes o cosas raras
+        # Se guarda en la variable que contiene toodo el fichero json de tipo string
+        conexion.close()
+        #Cierro la conexión con la pagina
+        info = json.loads(label)
+        # Cargas toodo el fichero json y lo guardas en una variable, y se lo coloca bien. Es la estructura del fichero
+        for i in range(len (info['results'])):
+            medicamento_info=info['results'][i]
+            print("El ID del medicamento",[i],"es: ", medicamento_info['id'])
+            if (medicamento_info['openfda']):
+                print("El medicamento lo fabrica:",medicamento_info['openfda']['manufacturer_name'][0])
+        if (len(info['results'])>100):
+            break
+        skip_number=skip_number+100
